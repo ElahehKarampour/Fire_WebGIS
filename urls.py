27666b0118c6519d929a12713ddeb114bdfaaedc
Elaheh Karampour <125409474+ElahehKarampour@users.noreply.github.com>
2025-03-19 23:10:10 +0330

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,31 @@
+"""webgis URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from gisapp.views import home_page,register,getWMSLayers,dashboard_view, guidance, about, space
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_page),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('register/',register),
+    path('wmslayers/',getWMSLayers),
+    path('dashboard/', dashboard_view),
+    path('guidance/', guidance),
+    path('about/', about),
+    path('space/', space),
+  
+]
